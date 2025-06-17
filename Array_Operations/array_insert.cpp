fc3f6a08452f7f27bbd7a956a93f0d9f8bc7f41d
Foysal79 <onthor312001@gmail.com>
2025-06-17 12:30:45 +0600

    first commit

---
+++ Array_Operations/array_insert.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int x;
+	cin >> x;
+	int i, n;
+	while(x--)
+	{
+	    cin >> n;
+	    cout << ((n * 24 )* 1000) << endl;
+	}
+
+
+
+    return 0;
+}

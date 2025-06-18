8862d094a5cacd418575c9aacded0259b6946cfd
Foysal79 <onthor312001@gmail.com>
2025-06-19 02:59:27 +0600

    insert at any index

--- Array_Operations/array_insert.cpp
+++ Array_Operations/array_insert.cpp
@@ -2,16 +2,27 @@
 using namespace std;
 int main()
 {
-	int x;
-	cin >> x;
-	int i, n;
-	while(x--)
-	{
-	    cin >> n;
-	    cout << ((n * 24 )* 1000) << endl;
-	}
+int n;
+cin >> n;
+int a[n+1];
+for(int i = 0; i < n; i++)
+{
+	cin >> a[i];
+}
 
+int index, val;
+cin >> index >> val;
 
+for( int i = n -1; i >= index; i--)
+{
+	a[i+1] = a[i];
+}
+a[index] = val;
+
+for(int i = 0; i < n+1; i++)
+{
+	cout << a[i] << " ";
+}
 
     return 0;
 }

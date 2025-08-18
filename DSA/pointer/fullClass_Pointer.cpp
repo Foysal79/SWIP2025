0af9ffeb5a642a728b03fda4e6726cfb1d963653
Foysal79 <onthor312001@gmail.com>
2025-08-18 06:22:52 +0600

    pointer

---
+++ DSA/pointer/fullClass_Pointer.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //* What is a Pointer?
+    /*A pointer is a special type of variable that stores the memory address of another variable. Instead of holding a value directly, it "points to" the location in memory where the value is stored. This allows for more powerful and flexible programming, particularly for tasks involving dynamic memory management and efficient data manipulation.*/
+
+
+    //* Declaring and Initializing Pointers
+    // int var = 10;
+    // int *ptr = &var;
+    // cout << "value of var : " << var << endl;
+    // cout << "Address of var : " << &var << endl;
+    // cout << "value of ptr  : " << ptr << endl;
+
+
+    //*Address-of (&) and Dereference (*) Operators
+    /*The address-of operator (&) is a unary operator that returns the memory address of its operand.
+    The dereference operator (*) is a unary operator that returns the value located at the memory address stored in the pointer.*/
+
+    // int var = 25;
+    // int *ptr = &var;
+    // cout << "Value of var: " << var << endl;
+    // cout << "Address stored in ptr: " << ptr << endl;
+    // cout << "Value at the address stored in ptr: " << *ptr << endl;
+    // // use the dereference operator to change the value
+    // *ptr = 50;
+    // cout << "New value of var : " << var << endl;
+
+
+    
+
+    return 0;
+}

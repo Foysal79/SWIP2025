f07142382ce27caacb49d90063c9c1e692a25a12
Foysal79 <onthor312001@gmail.com>
2025-08-18 13:19:14 +0600

    array class 1 full

--- DSA/Array_Operations/Array Operations - I/Array Operations - I.cpp
+++ DSA/Array_Operations/Array Operations - I/Array Operations - I.cpp
@@ -36,7 +36,82 @@ void reverseAnArray(int ar[], int n)
         cout << ar[i] << " ";
     }
 }
+void reverseAnArrayUsingTwoPointer(int ar[], int n)
+{
+    int i = 0, j = n - 1;
+    while (i < j)
+    {
+        swap(ar[i], ar[j]);
+        i++;
+        j--;
+    }
 
+    for (int k = 0; k < n; k++)
+    {
+        cout << ar[k] << " ";
+    }
+}
+
+void mergeTwoArrays(int ar[], int ar1[], int n, int m)
+{
+    int ar3[n + m];
+    for (int i = 0; i < n + m; i++)
+    {
+        if (i < n)
+        {
+            ar3[i] = ar[i];
+        }
+        else
+        {
+            ar3[i] = ar1[i - n];
+        }
+
+        cout << ar3[i] << " ";
+    }
+}
+
+void mergeTwoSortedArrays(int ar[], int ar1[], int n, int m)
+{
+    int c[n + m];
+    int k = 0, i = 0, j = 0;
+    while (i < n && j < m)
+    {
+        if (ar[i] < ar1[j])
+        {
+            c[k++] = ar[i];
+            i++;
+        }
+        else
+        {
+            c[k++] = ar1[j];
+            j++;
+        }
+    }
+    while (i < n)
+        c[k++] = ar[i++];
+    while (j < m)
+        c[k++] = ar1[j++];
+
+    for (int l = 0; l < m + n; l++)
+        cout << c[l] << " ";
+}
+
+void checkArrayIsSorted(int ar[], int n)
+{
+    int flag = 0;
+    for (int i = 0; i < n-1; i++)
+    {
+        if (ar[i] > ar[i + 1])
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 1)
+        cout << "Not Sort Array" << endl;
+    else
+        cout << "Sort Array" << endl;
+}
 int main()
 {
     //* Insert at Any Index
@@ -46,10 +121,24 @@ int main()
     for (int i = 0; i < n; i++)
         cin >> ar[i];
 
+    // second array input
+    // int m;
+    // cin >> m;
+    // int ar1[m];
+    // for (int i = 0; i < m; i++)
+    // {
+    //     cin >> ar1[i];
+    // }
+
     // int x, val;
     // cin >> x >> val;
     //* insertArray(ar, n, x, val);
     //* delateOperation(ar, n, x);
     //* reverseAnArray(ar, n);
+    //* reverseAnArrayUsingTwoPointer(ar, n);
+    //* mergeTwoArrays(ar, ar1, n, m);
+    //* mergeTwoSortedArrays(ar, ar1, n, m);
+    //* checkArrayIsSorted(ar, n);
+
     return 0;
 }

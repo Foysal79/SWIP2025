f59a5bf41a549a0eb3d381266c34719a0fa0703c
Foysal79 <onthor312001@gmail.com>
2025-08-25 07:03:20 +0600

    stl vector

---
+++ DSA/STL Vector/STL.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    //* vector initialisono
+    // vector<int> v;                  //only initial
+     vector<int> v = {1, 2, 3, 5};   // vector number initial
+    // vector<int> v(10);              //with size initial
+   // vector<int> v(10, 5);              //size with index value
+   
+    // cout << v.capacity() << endl;      // vector index size
+    // cout << v.size() << endl;           // vector element number
+
+    //  for(int i = 0; i < v.size(); i++)
+    //  {
+    //     cout << v[i] << " ";
+    //  }
+    
+    // for(int c : v)
+    // {
+    //     cout << c << " ";
+    // }
+
+    // cout << v.at(7) << endl;               // alert for range cross
+    // cout << v[7] << endl;                  // don't alert for range cross
+
+    
+
+
+
+    return 0;
+}

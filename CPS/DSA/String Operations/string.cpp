757be257467b214b3d09b38c796691989f81cff4
Foysal79 <onthor312001@gmail.com>
2025-08-26 13:05:43 +0600

    string finding folder name

--- CPS/DSA/String Operations/string.cpp
+++ CPS/DSA/String Operations/string.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main()
 {
 
-    string str;
-    getline(cin, str);
+    // string str;
+    // getline(cin, str);
     // cin >> str ;
     // str.push_back('e');
     // str += " World";
@@ -14,16 +14,49 @@ int main()
     // str.replace(6 , 9, "h");
     // sort(str.begin(), str.end(), greater<char>());
 
+    //* file name finding
 
-    //* file name finding 
+    // int idx = str.find_last_of('/');
+    // string str1 = str.substr(idx + 1, str.size());
 
-    int idx = str.find_last_of('/');
-     string str1 = str.substr(idx + 1 , str.size());
+    //* Count Frequency of Each Character
+    // string a, b;
+    // cin >> a >> b;
+    // vector<int> freq1(26, 0);
+    // vector<int> freq2(26, 0);
+
+    // for(char x : a)
+    // {
+    //      freq1[x - 'a']++; 
+    // }
+    // for(char x : b)
+    // {
+    //      freq2[x - 'a']++; 
+    // }
+    // bool ans = true;
+
+    // for(int i = 0; i < 26; i++)
+    // {
+    //     if(freq1[i] > 0)
+    //     {
+    //         cout << char('a' + i ) << " " << freq1[i] << endl;
+    //     }
+    // }
 
 
-     
+    //* vowel delate
+    
+    string s, r = "";
+    cin >> s;
 
-    cout << str1 << endl;
+    for(char c : s)
+    {
+        if(string("aeiouAEIOU").find(c) == -1)
+        {
+            r += c;
+        }
+    }
+    cout << r << endl;
 
     return 0;
 }

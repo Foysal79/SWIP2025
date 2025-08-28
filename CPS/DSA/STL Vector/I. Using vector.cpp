9d9fea4fc26dffcb06bc844317ef03f01749aa56
Foysal79 <onthor312001@gmail.com>
2025-08-28 07:08:09 +0600

    stack

--- CPS/DSA/STL Vector/I. Using vector.cpp
+++ CPS/DSA/STL Vector/I. Using vector.cpp
@@ -1,59 +1,53 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int main()
+int main ()
 {
-    int n, q;
-    cin >> n >> q;
-    vector<long long> v;
-    for (int i = 0; i < n; i++)
-    {
-        int x;
-        cin >> x;
-        v.push_back(x);
-    }
-
-    while(q--)
-    {
-        string s;
-        cin >> s;
-        if(s == "sort")
-        {
-            int x, y;
-            cin >> x >> y;
-            sort(v.begin()+(x-1), v.begin()+y);
-        }
-        if(s == "pop_back")
-        {
-            v.pop_back();
-        }
-        if(s == "back")
-        {
-            cout << v.back() << endl;
-        }
-        if(s == "reverse")
-        {
-            int x, y;
-            cin >> x >> y;
-            reverse(v.begin()+(x-1), v.begin()+y);
-        }
-        if(s == "front")
-        {
-            cout << v.front() << endl;
-        }
-        if(s == "push_back")
-        {
-            int x ;
-            cin >> x;
-
-            v.push_back(x);
-        }
-        if(s == "print")
-        {
-            int x;
-            cin >> x;
-            cout << v[x-1] << endl;
+    int size,query;
+    cin>>size>>query; // 8
+    string value;
+    cin>>value; // assiutinupperegypt
+    string qu;
+    int scope1,scope2;
+    while(query--){ // 8 7 6 5 4 3 2 1
+        cin>>qu;
+        if(qu == "pop_back"){
+            value.pop_back();
+        } else if (qu == "front"){
+            cout<<value.front()<<endl;
+        } else if (qu == "back"){
+            cout<<value.back()<<endl;
+        } else if (qu == "sort"){ 
+            //  01 2345 678
+             // ed cvbh dkk
+            //  12 3456 789
+            cin>>scope1>>scope2;  // 6 3
+            sort(value.begin()+ min(scope1,scope2) -1   // 2
+                ,value.begin()+max(scope1,scope2)); // 6
+        } else if (qu == "reverse"){
+            //  01 2345 678
+             // ed cvbh dkk
+            //  12 3456 789
+            cin>>scope1>>scope2;  // 6 3
+            reverse(value.begin()+ min(scope1,scope2) -1
+                ,value.begin()+max(scope1,scope2));
+        } else if (qu == "print"){
+            // 01234
+            //"eslam"
+            // 12345
+            int pos ;
+            cin>>pos;
+            cout<<value[pos - 1]<<endl;
+        } else if (qu == "substr"){
+            cin>>scope1>>scope2; // 4 1
+            for(int i = min(scope1,scope2) - 1;i<=max(scope1,scope2)-1;i++){
+                cout<<value[i];
+            }
+            cout<<endl;
+        } else {
+            //"bkjf"
+            char c;
+            cin>>c;
+            value.push_back(c);
         }
     }
-
-    return 0;
 }

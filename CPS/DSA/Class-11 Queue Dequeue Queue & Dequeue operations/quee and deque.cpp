d2d827aad0c7f17f411564a2473d73974684d845
Foysal79 <onthor312001@gmail.com>
2025-09-01 10:55:26 +0600

    deque stl

--- CPS/DSA/Class-11 Queue Dequeue Queue & Dequeue operations/quee and deque.cpp
+++ CPS/DSA/Class-11 Queue Dequeue Queue & Dequeue operations/quee and deque.cpp
@@ -3,23 +3,37 @@ using namespace std;
 int main(){
 
 //* Queue function || First is First Out (FIFO) , Last is Last Out (LILO)
- queue<int> q;
+//  queue<int> q;
+// q.push(10);
+// q.push(3);
+// q.push(50);
+// q.push(45);
+// q.push(38);
+// q.push(91);
+// cout << q.front() << endl;
+// cout << q.back() << endl; 
+// cout << q.size() << endl;
+// cout << endl;
+// while(!q.empty())
+// {
+//     cout << q.front() << " ";
+//     q.pop(); 
+// }
 
-q.push(10);
-q.push(3);
-q.push(50);
-q.push(45);
-q.push(38);
-q.push(91);
+//* DeQueue || vector behabier || both side end stile Queue style
 
-cout << q.front() << endl;
-cout << q.back() << endl; 
-cout << q.size() << endl;
-cout << endl;
-while(!q.empty())
+deque<int> dq;
+dq.push_back(10);
+dq.push_back(345);
+dq.push_back(56);
+dq.push_back(23);
+dq.push_back(89);
+dq.push_front(3);
+
+
+for(int x : dq)
 {
-    cout << q.front() << " ";
-    q.pop(); 
+   cout << x << " ";
 }
 
 }
